de99a722351835d4e8df5abffa1cb480d2d9bba5
yruano <ckrmidjskk@naver.com>
2024-11-13 20:24:39 +0900

    Updates

---
+++ _app/immutable/chunks/paths.CpopgJhi.js
@@ -0,0 +1,1 @@
+var s;const o=((s=globalThis.__sveltekit_h7sot5)==null?void 0:s.base)??"/Auto_Lotto";var t;const a=((t=globalThis.__sveltekit_h7sot5)==null?void 0:t.assets)??o;export{a,o as b};
---
+++ _app/immutable/entry/start.CaD3aCPk.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CGZvY0Wk.js";export{t as start};
